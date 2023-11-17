467f19e32246432a3a8e75630a24e4ecc43c8aff
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-11-17 17:25:55 +0100

    style: add transition to Article and update placing

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import Article from "./components/Article";
 
 function App() {
   return (
-    <div className=" w-full h-screen bg-linen text-raisin-black">
+    <div className="flex flex-col w-full h-screen bg-linen text-raisin-black">
       <Header />
-      <div className="grid grid-rows-5 grid-cols-4 justify-items-center place-content-center">
-        <Article className="w-[450px] h-[250px]" value="Article One" />
-        <Article className="w-[250px] h-[350px]" value="Article Two" />
-        <Article className="w-[250px] h-[250px]" value="Article Three" />
-        <Article className="w-[250px] h-[250px]" value="Article Four" />
-        <Article className="w-[250px] h-[250px]" value="Article Five" />
-        <Article className="w-[250px] h-[250px]" value="Article Six" />
+      <div className="h-full grid grid-rows-5 grid-cols-4 justify-items-center place-content-center p-2 gap-2">
+        <Article className="w-full h-full col-span-2" value="Article One" />
+        <Article className="w-full h-full row-span-2" value="Article Two" />
+        <Article className="w-full h-full" value="Article Three" />
+        <Article className="w-full h-full" value="Article Four" />
+        <Article className="w-full h-full" value="Article Five" />
+        <Article className="w-full h-full" value="Article Six" />
+        <Article
+          className="w-full h-full col-span-3 row-span-2"
+          value="Article Seven"
+        />
+        <Article className="w-full h-full row-span-2" value="Article Eight" />
+        <Article className="w-full h-full col-span-4 " value="Article Nine" />
       </div>
     </div>
   );
--- src/components/Article.js
+++ src/components/Article.js
@@ -2,7 +2,7 @@ import classNames from "classnames";
 
 function Article({ className, value }) {
   const articleClasses = classNames(
-    "bg-persian-red rounded-xl text-center text-2xl",
+    "bg-persian-red rounded-xl text-center text-2xl hover:bg-persian-red/90 transition-colors",
     className
   );
 

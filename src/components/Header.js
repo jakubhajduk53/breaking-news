ea2b3d6aaec03ea9b348b07644d6c2a0719912b7
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-11-18 17:34:49 +0100

    feat: add text to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
 function Header() {
-  return <div className="w-full h-24 bg-verdigris shadow-md"></div>;
+  return (
+    <div className="w-full h-24 bg-verdigris shadow-md text-4xl flex items-center pl-10">
+      Homepage
+    </div>
+  );
 }
 
 export default Header;

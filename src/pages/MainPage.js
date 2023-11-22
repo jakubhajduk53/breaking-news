327706d85924d26339d1c1d747a7d550c7d2b0fa
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-11-22 16:37:30 +0100

    chore: change articles display

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -5,19 +5,67 @@ function MainPage() {
   const [isOpened, setIsOpened] = useState(false);
   const [activeArticle, setActiveArticle] = useState("");
 
+  const articles = [
+    {
+      id: 1,
+      additionalClasses: "col-span-2",
+      value: "Article One",
+    },
+    {
+      id: 2,
+      additionalClasses: "row-span-2",
+      value: "Article Two",
+    },
+    {
+      id: 3,
+      additionalClasses: "",
+      value: "Article Three",
+    },
+    {
+      id: 4,
+      additionalClasses: "",
+      value: "Article Four",
+    },
+    {
+      id: 5,
+      additionalClasses: "",
+      value: "Article Five",
+    },
+    {
+      id: 6,
+      additionalClasses: "",
+      value: "Article Six",
+    },
+    {
+      id: 7,
+      additionalClasses: "col-span-3 row-span-2",
+      value: "Article Seven",
+    },
+    {
+      id: 8,
+      additionalClasses: "row-span-2",
+      value: "Article Eight",
+    },
+    {
+      id: 9,
+      additionalClasses: "col-span-4",
+      value: "Article Nine",
+    },
+  ];
+
   return (
     <div className="w-full h-full">
       {!isOpened ? (
         <div className="h-full grid grid-rows-5 grid-cols-4 justify-items-center place-content-center p-2 gap-2">
-          <Article className="col-span-2" value="Article One" />
-          <Article className="row-span-2" value="Article Two" />
-          <Article value="Article Three" />
-          <Article value="Article Four" />
-          <Article value="Article Five" />
-          <Article value="Article Six" />
-          <Article className="col-span-3 row-span-2" value="Article Seven" />
-          <Article className="row-span-2" value="Article Eight" />
-          <Article className="col-span-4 " value="Article Nine" />
+          {articles.map((article) => {
+            return (
+              <Article
+                key={article.id}
+                className={article.additionalClasses}
+                value={article.value}
+              />
+            );
+          })}
         </div>
       ) : (
         <div className="w-full h-full bg-persian-red">{activeArticle}</div>

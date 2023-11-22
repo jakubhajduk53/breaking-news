7df2af1e33a7c9442fe3718dd876bff06e8f97f9
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-11-22 16:33:42 +0100

    feat: add new flags to MainPage

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,16 @@
 import classNames from "classnames";
 
-function Article({ className, value }) {
+function Article({ className, value, onClick }) {
   const articleClasses = classNames(
     "w-full h-full  bg-persian-red rounded-xl text-center text-2xl hover:bg-persian-red/90 transition-colors cursor-pointer",
     className
   );
 
-  return <div className={articleClasses}>{value}</div>;
+  return (
+    <div className={articleClasses} onClick={onClick}>
+      {value}
+    </div>
+  );
 }
 
 export default Article;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import Article from "../components/Article";
 
 function MainPage() {
+  const [isOpened, setIsOpened] = useState(false);
+  const [activeArticle, setActiveArticle] = useState("");
+
   return (
-    <div className="h-full grid grid-rows-5 grid-cols-4 justify-items-center place-content-center p-2 gap-2">
-      <Article className="col-span-2" value="Article One" />
-      <Article className="row-span-2" value="Article Two" />
-      <Article value="Article Three" />
-      <Article value="Article Four" />
-      <Article value="Article Five" />
-      <Article value="Article Six" />
-      <Article className="col-span-3 row-span-2" value="Article Seven" />
-      <Article className="row-span-2" value="Article Eight" />
-      <Article className="col-span-4 " value="Article Nine" />
+    <div className="w-full h-full">
+      {!isOpened ? (
+        <div className="h-full grid grid-rows-5 grid-cols-4 justify-items-center place-content-center p-2 gap-2">
+          <Article className="col-span-2" value="Article One" />
+          <Article className="row-span-2" value="Article Two" />
+          <Article value="Article Three" />
+          <Article value="Article Four" />
+          <Article value="Article Five" />
+          <Article value="Article Six" />
+          <Article className="col-span-3 row-span-2" value="Article Seven" />
+          <Article className="row-span-2" value="Article Eight" />
+          <Article className="col-span-4 " value="Article Nine" />
+        </div>
+      ) : (
+        <div className="w-full h-full bg-persian-red">{activeArticle}</div>
+      )}
     </div>
   );
 }

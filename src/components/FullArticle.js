68fdbed6e241770003c583b20895614936d6fcd3
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-11-25 15:52:21 +0100

    style: update value paragraph

--- src/components/FullArticle.js
+++ src/components/FullArticle.js
@@ -1,10 +1,10 @@
 function FullArticle({ value, onClick }) {
   return (
     <div className="w-full h-full grid grid-rows-5 grid-cols-4 justify-items-center place-content-center p-2 gap-2">
-      <div className="w-full h-full row-span-4 col-span-4 bg-persian-red rounded-xl text-center text-2xl">
+      <div className="w-full h-full flex flex-col row-span-4 col-span-4 bg-persian-red rounded-xl text-center text-2xl">
         <p className=" text-2xl">{value.title}</p>
         <p className=" text-xl">{value.subtitle}</p>
-        <p className=" text-lg border ml-5 mr-5 rounded-xl mt-2">
+        <p className=" w-[70%] self-center text-lg shadow-xl ml-5 mr-5 rounded-xl mt-5 ">
           {value.value}
         </p>
       </div>

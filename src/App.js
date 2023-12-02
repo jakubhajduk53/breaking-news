ed0bf40881e75ff5f1033f40332c32db1a1c5a68
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-12-02 17:24:20 +0100

    feat: fill Contact page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import MainPage from "./pages/MainPage";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Routes>
         <Route element={<MainPage />} path="/" />
+        <Route element={<Contact />} path="/contact" />
       </Routes>
     </div>
   );
---
+++ src/pages/Contact.js
@@ -0,0 +1,20 @@
+function Contact() {
+  return (
+    <div className="text-linen flex flex-col items-center text-3xl">
+      <p>
+        Created by: <span className="text-verdigris">Jakub Hajduk</span>
+      </p>
+      <p>
+        Email: <span className="text-verdigris">jakubhajduk53@gmail.com</span>
+      </p>
+      <p>
+        Api: <span className="text-verdigris">https://newsapi.org/</span>
+      </p>
+      <p>
+        Repository: <span className="text-verdigris"></span>
+      </p>
+    </div>
+  );
+}
+
+export default Contact;

88565107012512c7451da9d28d163544bd113650
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-12-03 15:55:40 +0100

    style: change title color

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,13 +2,13 @@ import classNames from "classnames";
 
 function Article({ className, onClick, description, title }) {
   const articleClasses = classNames(
-    "w-full h-full flex flex-col bg-persian-red rounded-xl text-center hover:bg-persian-red/90 transition-colors cursor-pointer text-2xl",
+    "w-full h-full flex flex-col bg-persian-red rounded-xl text-center hover:bg-persian-red/90 transition-colors cursor-pointer text-2xl ",
     className
   );
 
   return (
     <div className={articleClasses} onClick={onClick}>
-      <p>{title}</p>
+      <p className="text-linen/70">{title}</p>
       <p className=" text-xl pl-5 pr-5 overflow-hidden">{description}</p>
     </div>
   );
